0d86bcd8f9a091e583ac5b16756077bea4c571c5
Gabriel Tan <gabriel.tance@shopee.com>
2023-05-21 16:33:56 +0800

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Blank Slate - React Vite
+
+A personal barebones React starter to jumpstart projects.
+
+## Quickstart
+
+`degit git@github.com:Caephler/blank-slate-react-vite.git your-project-name`
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <section className="w-full h-full flex justify-center items-center flex-col">
+        <h1 className="text-3xl">Hello World</h1>
+        <article className="prose prose-slate">Subtitle</article>
+      </section>
+    </div>
+  );
+}
+
+export default App;
